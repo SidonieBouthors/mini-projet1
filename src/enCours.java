d2316c5f4d549b83aa8040d2abc29b13f0fcd21b
ZiZouet <91062845+zizouet@users.noreply.github.com>
2021-10-27 09:26:17 +0200

    getneighbours

--- src/enCours.java
+++ src/enCours.java
@@ -1,10 +1,30 @@
 public class enCours {
+    public static void main(String[] args) {
+        boolean [] tab = new boolean[3];
+        for (int i = 0; i < 3; i++) {
+            if (tab[i] == false) {
+                System.out.println("yes");
+            }
+        }
 
+    }
 
 
     public static boolean[] getNeighbours(boolean[][] image, int row, int col) {
         assert (image != null);
         boolean[] returningTab = new boolean[8]; //Le tableau final retourné qui getNeighbours
+
+        if (col + 1 <= 99 && row + 1 <= 99 && col - 1 >= 0 && row - 1 >= 0) {
+            returningTab[0] = image[row - 1][col];
+            returningTab[6] = image[row][col - 1];
+            returningTab[7] = image[row - 1][col-1];
+            returningTab[4] = image[row + 1][col];      //si le coin superieur gauche et le coin inferieur droit sont libres alors tout est libre
+            returningTab[3] = image[row + 1][col + 1];  //donc on run seuleument lui. Ce qui devrait fonctionner dans la plupart des cas.Cela me semble un peu plus opti
+            returningTab[1] = image[row - 1][col+1];
+            returningTab[2] = image[row][col+1];
+            returningTab[5] = image[row - 1][col-1];
+            return returningTab
+        }
 
         if (row - 1 >= 0) {                         // Check d'indice Au milieu en haut / tab indice 0
             returningTab[0] = image[row - 1][col];
